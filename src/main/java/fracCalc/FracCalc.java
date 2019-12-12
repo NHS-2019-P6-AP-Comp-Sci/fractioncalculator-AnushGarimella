ad5062779373aafae32e70b3b8a189306a1edc96
s-garimellaan <s-garimellaan@53S50030255.it.bsd405.org>
2019-12-12 08:05:25 -0800

    I passed checkpoint 3's tests

--- src/main/java/fracCalc/FracCalc.java
+++ src/main/java/fracCalc/FracCalc.java
@@ -45,26 +45,116 @@ public class FracCalc {
 		String firstOperand = math.next();
 		String operator = math.next();
 		String secondOperand = math.next();
-		String result = "";		
-		String whole = "whole:";
-		String numerator = " numerator:";
-		String denominator = " denominator:";;
-		int x = secondOperand.indexOf("_");
-		int y = secondOperand.indexOf("/");
-		if (!secondOperand.contains("_") && !secondOperand.contains("/")) {
-			result += whole + secondOperand;
-			result += numerator + 0;
-			result += denominator + 1; 
-		} else if (!secondOperand.contains("_")) {
-			result += whole + 0;
-			result += numerator + secondOperand.substring(x + 1, y);
-			result += denominator + secondOperand.substring(y + 1, secondOperand.length());
-		} else {
-			result += (whole + secondOperand.substring(0, x));
-			result += (numerator + secondOperand.substring(x + 1, y));
-			result += (denominator + secondOperand.substring(y + 1, secondOperand.length()));
+		/*
+		 * String result = ""; String whole = "whole:"; String numerator =
+		 * " numerator:"; String denominator = " denominator:";
+		 */
+		int w1 = 0;
+		int num1 = 0;
+		int den1 = 0;
+		if (firstOperand.contains("_") && firstOperand.contains("/")) {
+			w1 += Integer.valueOf(firstOperand.substring(0, firstOperand.indexOf('_')));
+			num1 += Integer.valueOf(firstOperand.substring(firstOperand.indexOf('_') + 1, firstOperand.indexOf('/')));
+			den1 += Integer.valueOf(firstOperand.substring(firstOperand.indexOf('/') + 1, firstOperand.length()));
+		} else if (!firstOperand.contains("_") && firstOperand.contains("/")) {
+			w1 += 0;
+			num1 += Integer.valueOf(firstOperand.substring(0, firstOperand.indexOf("/")));
+			den1 += Integer.valueOf(firstOperand.substring(firstOperand.indexOf('/') + 1, firstOperand.length()));
+		} else if (!firstOperand.contains("_") && !firstOperand.contains("/")) {
+			w1 = Integer.valueOf(firstOperand.substring(0, firstOperand.length()));
+			num1 = 0;
+			den1 = 1;
 		}
-		return result;
+		int w2 = 0;
+		int num2 = 0;
+		int den2 = 0;
+		if (secondOperand.contains("_") && secondOperand.contains("/")) {
+			w2 += Integer.valueOf(secondOperand.substring(0, secondOperand.indexOf('_')));
+			num2 += Integer
+					.valueOf(secondOperand.substring(secondOperand.indexOf('_') + 1, secondOperand.indexOf('/')));
+			den2 += Integer.valueOf(secondOperand.substring(secondOperand.indexOf('/') + 1, secondOperand.length()));
+		} else if (!secondOperand.contains("_") && secondOperand.contains("/")) {
+			w2 += 0;
+			num2 += Integer.valueOf(secondOperand.substring(0, secondOperand.indexOf("/")));
+			den2 += Integer.valueOf(secondOperand.substring(secondOperand.indexOf('/') + 1, secondOperand.length()));
+		} else if (!secondOperand.contains("_") && !secondOperand.contains("/")) {
+			w2 += Integer.valueOf(secondOperand.substring(0, secondOperand.length()));
+			num2 += 0;
+			den2 += 1;
+		}
+		int w3 = 0;
+		int num3 = 0;
+		int den3 = 0;
+		if (w1 < 0) {
+			num1 = (den1 * w1) - num1;
+		}else {
+			num1 = (den1 * w1) + num1;
+			
+		}
+		if (w2 < 0) {
+			num2 = (den2 * w2) - num2;
+		}else {
+			num2 = (den2 * w2) + num2;
+		}
+		if (operator.contentEquals("+")) {
+			num3 += ((num1 * den2) + (num2 * den1));
+			den3 += (den1 * den2);
+			if (den1 == den2) {
+				num3 = num1 + num2;
+				den3 = den2;
+			}
+
+		} else if (operator.contentEquals("-")) {
+			num3 += (num1 * den2) - (num2 * den1);
+			den3 += (den1 * den2);
+			if (den1 == den2) {
+				num3 = num1 - num2;
+				den3 = den2;
+			}
+				
+		} else if (operator.contentEquals("*")) {
+			num3 += (num1 * num2);
+			den3 += (den1 * den2);
+			if (w1 == 0 || w2 == 0) {
+				w3 = 0;
+			}
+		} else if (operator.equals("/")) {
+			num3 += (num1 * den2);
+			den3 += (den1 * num2);
+			if (num2 == 0) {
+				return "Cannot divide by 0";
+			}
+			if (firstOperand.contentEquals("0")) {
+				return "0";
+			}
+		}
+
+		/*
+		 * int x = secondOperand.indexOf("_"); int y = secondOperand.indexOf("/"); if
+		 * (!secondOperand.contains("_") && !secondOperand.contains("/")) { result +=
+		 * whole + secondOperand; result += numerator + 0; result += denominator + 1; }
+		 * else if (!secondOperand.contains("_")) { result += whole + 0; result +=
+		 * numerator + secondOperand.substring(x + 1, y); result += denominator +
+		 * secondOperand.substring(y + 1, secondOperand.length()); } else { result +=
+		 * (whole + secondOperand.substring(0, x)); result += (numerator +
+		 * secondOperand.substring(x + 1, y)); result += (denominator +
+		 * secondOperand.substring(y + 1, secondOperand.length())); }
+		 */
+		String whole = String.valueOf(w3);
+		String numerator = String.valueOf(num3);
+		String denominator = String.valueOf(den3);
+		String answer = numerator + "/" + denominator;
+		if (num3 == 0 && den3 == 0 && operator.contentEquals("+")) {
+			answer = String.valueOf(w1 + w2);
+		} else if (num3 == 0 && den3 == 0 && operator.contentEquals("-")) {
+			answer = String.valueOf(w1 - w2);
+		} else if (num3 == 0 && den3 == 0 && operator.contentEquals("*")) {
+			answer = String.valueOf(w1 * w2);
+		} else if (num3 == 0 && den3 == 0 && operator.contentEquals("/")) {
+			answer = String.valueOf(w1 / w2);
+		}
+
+		return answer;
 	}
 
 	// TODO: Fill in the space below with any helper methods that you think you will
